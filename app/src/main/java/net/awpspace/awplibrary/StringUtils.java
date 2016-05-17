4e4e6e7eb3abfb0cc654e61640419ef1dae385f2
HaiNguyen <tuanhaicntt@gmail.com>
2016-05-17 11:53:28 +0700

    Initialize project

---
+++ app/src/main/java/net/awpspace/awplibrary/StringUtils.java
@@ -0,0 +1,14 @@
+package net.awpspace.awplibrary;
+
+import android.util.Log;
+
+/**
+ * Created by tuanhai on 5/17/16.
+ */
+public enum StringUtils {
+    Instance;
+
+    public void function() {
+        Log.e("haint", "ahihii!");
+    }
+}
